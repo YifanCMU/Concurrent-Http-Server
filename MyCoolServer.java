d7cae04e5df3d3c5c525d4954208c0376c83a9d0
YifanCMU <yifanw3@andrew.cmu.edu>
2016-04-06 00:27:53 -0400

    Update MyCoolServer.java

--- MyCoolServer.java
+++ MyCoolServer.java
@@ -1,66 +1,92 @@
 package mycoolserver;
-
 /**
  * a Concurrent HTTP server
  *
  * @author Yifan Wang Please test the server by using web browser to visit
- * "localhost:7777/filename" e.g. "localhost:/7777/MadaData.txt" If the file you
- * want is under the same directory as this piece of code, then the content of
- * the file will be returned to your web browser
+ * "localhost:7777/filename" e.g. "localhost:/7777/MadaData.txt"
  */
+
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
-
 public class MyCoolServer {
-
-    public static void main(String[] args) {
+    Scanner filescanner = null;
+    static ServerSocket listenSocket=null;
+    static Socket clientSocket = null;
+    public static void main(String[] args) throws Exception{
+        MyCoolServer myserver = new MyCoolServer();
         while (true) {
-            Socket clientSocket = null;
-            PrintWriter out = null;
-            ServerSocket listenSocket = null;
-            Scanner filescanner = null;
-            try {
-                int serverPort = 7777; //port number
-                listenSocket = new ServerSocket(serverPort);
-                clientSocket = listenSocket.accept();
-                Scanner in;
-                in = new Scanner(clientSocket.getInputStream());
-                out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())));
-                String line = in.nextLine();
-                String filename = line.split("/")[1];
-                filename = filename.substring(0, filename.length() - 5); //get file name        
+            int serverPort = 7777; //port number
+            listenSocket = new ServerSocket(serverPort);   
+            clientSocket = listenSocket.accept();        
+            Scanner in;
+            in = new Scanner(clientSocket.getInputStream()); 
+            String line = in.nextLine();              
+            String filename = line.split("/")[1];
+            filename = filename.substring(0, filename.length() - 5); //get file name   
+            System.out.println("file name:"+filename);
+            if(filename.endsWith(".jpg")){
+                myserver.sendPic(filename,clientSocket);
+            }
+            else{
+                myserver.sendText(filename,clientSocket);
+            }
+            close();
+            
+            }
+    }
+    private void sendPic(String path, Socket client) {
+    File fileToSend = new File(path);
+    if (fileToSend.exists() && !fileToSend.isDirectory()) {
+      try {
+        PrintStream writer = new PrintStream(client.getOutputStream());
+        writer.println("HTTP/1.0 200 OK");
+        writer.println("Content-Type:application/binary");
+        writer.println("Content-Length:" + fileToSend.length());
+        writer.println();
+        FileInputStream fis = new FileInputStream(fileToSend);
+        byte[] buf = new byte[fis.available()];
+        fis.read(buf);
+        writer.write(buf);
+        writer.close();
+        fis.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+   
+    public void sendText(String filename, Socket client) throws Exception{
+            PrintWriter printwriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())));          
+            try{
                 File file = new File(filename);
-                filescanner = new Scanner(file);
-                String output = "HTTP/1.1 200 OK\n";
+                filescanner = new Scanner(file,"UTF-8");                             
                 while (filescanner.hasNextLine()) {
                     String data = filescanner.nextLine();
-                    //System.out.println(data);
-                    //content = content+data+"\n";
-                    out.write(data + "\n");
-                }
-                out.flush();
-            } catch (IOException e) {
-                System.out.println("IO Exception:" + e.getMessage());
-                out.println("HTTP/1.1 404 not found\n");
-                out.println("File does not exist");
-                out.flush();
-            } finally {
-                try {
-                    if (clientSocket != null) {
-                        if (filescanner != null) {
-                            filescanner.close();
-                        }
-                        listenSocket.close();
-                        clientSocket.close();
-
-                    }
-                } catch (IOException e) {
-                    System.out.println("Exception:" + e.getMessage());
-                }
-
+                    printwriter.write(data+"\n");                   
+                }               
+                //out.flush();
+                printwriter.flush();
             }
-        }
+            catch (IOException e) {
+                System.out.println("IO Exception:" + e.getMessage());               
+                printwriter.write("HTTP/1.1 404 not found\n");
+                printwriter.write("File does not exist");
+                printwriter.flush();
+            } 
+            finally{
+                 if (filescanner != null) {
+                        filescanner.close();
+                }
+            }
     }
-
+    public static void close(){    
+        try{       
+            listenSocket.close();
+            clientSocket.close();
+        } 
+        catch (IOException e) {
+            System.out.println("Exception:" + e.getMessage());
+        }           
+    }
 }
